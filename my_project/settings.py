cdd47db47f7a8ad4b6258dfe076131cfd1d83a27
Zalak Nrupay Shah <oel232ec@students.codeinstitute.net>
2025-01-14 15:45:49 +0000

    setting.py debug=false add

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%o#3*gm^-(icgk2171(&#faqm9vfmrvvcm1kt_%u23i73gd^v2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
